ffa10f6c2d47646483b2190b55ffa089e72c3285
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-05 15:56:35 +0530

    Added logic to "Check for Winner" & Redirecting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+    $winner = "random"
 end
--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,12 +1,56 @@
 class MovesController < ApplicationController
+
+    before_action :check_for_winner
+
     def create 
-        @game = Game.find(params[:game_id])
-        @game.move(params[:row], params[:col])
 
         respond_to do |format|
             format.html {
                 redirect_to @game
             }
+        end
+    end
+
+    def check_for_winner
+
+        if $winner == "x" || $winner == "o"
+            redirect_to "/games"
+        end
+        
+        @game = Game.find(params[:game_id])
+        @game.move(params[:row], params[:col])
+
+        if @game.state[0.to_s][0.to_s] == @game.state[0.to_s][1.to_s] && @game.state[0.to_s][1.to_s] == @game.state[0.to_s][2.to_s] 
+            logger.info "======================  #{@game.state}"
+            $winner = @game.state[0.to_s][0.to_s]
+
+        elsif @game.state[1.to_s][0.to_s] == @game.state[1.to_s][1.to_s] && @game.state[1.to_s][1.to_s] == @game.state[1.to_s][2.to_s] 
+            logger.info "======================  #{@game.state}"
+            $winner = @game.state[1.to_s][0.to_s]
+
+        elsif @game.state[2.to_s][0.to_s] == @game.state[2.to_s][1.to_s] && @game.state[2.to_s][1.to_s] == @game.state[2.to_s][2.to_s] 
+            logger.info "======================  #{@game.state}"
+            $winner = @game.state[2.to_s][0.to_s]
+
+        elsif @game.state[0.to_s][0.to_s] == @game.state[1.to_s][0.to_s] && @game.state[1.to_s][0.to_s] == @game.state[2.to_s][0.to_s]  
+            logger.info "======================  #{@game.state}"
+            $winner = @game.state[0.to_s][0.to_s]
+
+        elsif @game.state[0.to_s][1.to_s] == @game.state[1.to_s][1.to_s] && @game.state[1.to_s][1.to_s] == @game.state[2.to_s][1.to_s]
+            logger.info "======================  #{@game.state}"
+            $winner = @game.state[0.to_s][1.to_s]
+
+        elsif @game.state[2.to_s][0.to_s] == @game.state[1.to_s][2.to_s] && @game.state[1.to_s][2.to_s] == @game.state[2.to_s][2.to_s] 
+            logger.info "======================  #{@game.state}"
+            $winner = @game.state[2.to_s][0.to_s]
+            
+        elsif @game.state[0.to_s][0.to_s] == @game.state[1.to_s][1.to_s] && @game.state[1.to_s][1.to_s] == @game.state[2.to_s][2.to_s] 
+            logger.info "======================  #{@game.state}"
+            $winner = @game.state[0.to_s][0.to_s]
+
+        elsif @game.state[0.to_s][2.to_s] == @game.state[1.to_s][1.to_s] && @game.state[1.to_s][1.to_s] == @game.state[2.to_s][0.to_s]
+            logger.info "======================  #{@game.state}" 
+            $winner = @game.state[0.to_s][2.to_s]
         end
     end
 end
