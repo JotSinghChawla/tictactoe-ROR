c91e3a81632434790be2649fdaebc26c29e0d2be
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-05 12:20:03 +0530

    Added "New Game" working link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'games/index'
-  get 'games/show'
+  resources :games
+  root to: 'games#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
