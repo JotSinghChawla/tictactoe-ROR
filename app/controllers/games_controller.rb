fe70b11b88c44e947e4d944b4c33d1cf9c04e7a2
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-06 10:43:48 +0530

    Added playerx & playero form in home page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+
   def index
     @games = Game.all
   end
@@ -8,7 +9,24 @@ class GamesController < ApplicationController
   end
 
   def create 
-    # @game = Game.new(game_params, state: state, current_symbol: current_symbol)
-    redirect_to Game.create
+    @game = Game.new(game_params)
+    if @game.save then redirect_to @game end
   end
+
+  private
+
+  def game_params
+    # logger.info "\nLOL\n #{params[:state]} \n\n"
+
+    params
+    .permit(:playero, :playerx)
+    # .require(:game)
+    
+  #   .reverse_merge(state: { 
+  #     0 => { 0 => nil, 1 => nil, 2 => nil },
+  #     1 => { 0 => nil, 1 => nil, 2 => nil },
+  #     2 => { 0 => nil, 1 => nil, 2 => nil }
+  # }, match_winner: "NO one", current_symbol: "o")
+  end
+
 end
